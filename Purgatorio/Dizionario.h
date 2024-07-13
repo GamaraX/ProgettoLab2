aaef791703b0a13b02529cdbce072b35a7605a3e
GamaraX <lorins709@gmail.com>
2024-07-14 01:00:37 +0200

    Aggiornamento Dizionario

--- Purgatorio/Dizionario.h
+++ Purgatorio/Dizionario.h
@@ -1,2 +1,4 @@
 //
-int Ricerca_Binaria_Dizionario(char* file, char* parola_utente);
+int Ricerca_Binaria_Dizionario(char* parole[], int conteggio, const char *parolaDaCercare);
+int Carica_Dizionario(const char *nomeFile, char* parole[]);
+void Dealloca_Dizionario(char* parole[], int conteggio);
